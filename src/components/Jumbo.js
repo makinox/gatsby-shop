0a5cb4b033b2ecca37dcfd8eca0262282af0c4ee
Jesus bossa <x@jesusbossa.com>
2020-04-15 17:54:07 -0500

    [INIT]

---
+++ src/components/Jumbo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyledJumbo } from '../styles/components'
+
+export default function Jumbo() {
+
+    return (
+        <div>
+            <StyledJumbo>
+                <div>
+                    <h2>Consige el mejor swag exclusivo y expecial de platzi</h2>
+                    <small>Piezas elegantes para los mejores estudiantes</small>
+                </div>
+            </StyledJumbo>
+        </div>
+    )
+}
