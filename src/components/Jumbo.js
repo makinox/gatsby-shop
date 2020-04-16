99f8d08eb4658ffd28d92f52f930a9fd2a680fcd
Jesus bossa <x@jesusbossa.com>
2020-04-16 15:32:59 -0500

    [ADD] GRAPH

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { StyledJumbo } from '../styles/components'
 
-export default function Jumbo() {
+export default function Jumbo({description}) {
 
     return (
         <div>
             <StyledJumbo>
                 <div>
                     <h2>Consige el mejor swag exclusivo y expecial de platzi</h2>
-                    <small>Piezas elegantes para los mejores estudiantes</small>
+                    <small>{description}</small>
                 </div>
             </StyledJumbo>
         </div>
