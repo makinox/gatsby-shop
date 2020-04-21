80e6b7559e4b4bfd08c2ed9e620d1d1bff0095a8
Jesus bossa <x@jesusbossa.com>
2020-04-21 15:55:27 -0500

    [ADD] Build config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,11 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+const React = require("react");
+const Layout = require("./src/components/layout").default;
+const { GlobalStyles } = require("./src/styles");
+const { CartProvider } = require("./src/context");
 
-// You can delete this file if you're not using it
-
-import React from 'react'
-import Layout from './src/components/layout'
-import { GlobalStyles } from './src/styles'
-import { CartProvider } from './src/context'
-
-export function wrapRootElement({ element }) { return (
-    <CartProvider>
-        <GlobalStyles />
-        <Layout>
-            {element}
-        </Layout>
-    </CartProvider>
-)}
+exports.wrapRootElement = ({ element }) => (
+  <CartProvider>
+    <GlobalStyles />
+    <Layout>{element}</Layout>
+  </CartProvider>
+);
